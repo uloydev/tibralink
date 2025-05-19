a77660cd93a11a97ff37eb0a5c3831d1a583af49
wahyu miftahul aflah <uloydev@gmail.com>
2025-05-19 20:15:38 +0700

    disable register user feature

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -59,41 +59,41 @@ export const actions: Actions = {
 
     return redirect(302, "/dashboard");
   },
-  register: async (event) => {
-    const formData = await event.request.formData();
-    const username = formData.get("username");
-    const password = formData.get("password");
+  // register: async (event) => {
+  //   const formData = await event.request.formData();
+  //   const username = formData.get("username");
+  //   const password = formData.get("password");
 
-    if (!validateUsername(username)) {
-      return fail(400, { message: "Invalid username" });
-    }
-    if (!validatePassword(password)) {
-      return fail(400, { message: "Invalid password" });
-    }
+  //   if (!validateUsername(username)) {
+  //     return fail(400, { message: "Invalid username" });
+  //   }
+  //   if (!validatePassword(password)) {
+  //     return fail(400, { message: "Invalid password" });
+  //   }
 
-    const userId = generateUserId();
-    const passwordHash = await hash(password, {
-      // recommended minimum parameters
-      memoryCost: 19456,
-      timeCost: 2,
-      outputLen: 32,
-      parallelism: 1,
-    });
+  //   const userId = generateUserId();
+  //   const passwordHash = await hash(password, {
+  //     // recommended minimum parameters
+  //     memoryCost: 19456,
+  //     timeCost: 2,
+  //     outputLen: 32,
+  //     parallelism: 1,
+  //   });
 
-    try {
-      const db = await getDB();
-      await db
-        .insert(table.user)
-        .values({ id: userId, username, passwordHash });
+  //   try {
+  //     const db = await getDB();
+  //     await db
+  //       .insert(table.user)
+  //       .values({ id: userId, username, passwordHash });
 
-      const sessionToken = auth.generateSessionToken();
-      const session = await auth.createSession(sessionToken, userId);
-      auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
-    } catch (e) {
-      return fail(500, { message: "An error has occurred" });
-    }
-    return redirect(302, "/dashboard");
-  },
+  //     const sessionToken = auth.generateSessionToken();
+  //     const session = await auth.createSession(sessionToken, userId);
+  //     auth.setSessionTokenCookie(event, sessionToken, session.expiresAt);
+  //   } catch (e) {
+  //     return fail(500, { message: "An error has occurred" });
+  //   }
+  //   return redirect(302, "/dashboard");
+  // },
 };
 
 function generateUserId() {
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -64,14 +64,14 @@
 						Sign in
 					</button>
 				</div>
-				<div>
+				<!-- <div>
 					<button
 						formaction="?/register"
 						class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
 					>
 						register
 					</button>
-				</div>
+				</div> -->
 			</form>
 		</div>
 	</div>
